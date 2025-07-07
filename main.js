b543f3716ecc253265f725528d609fba738106a4
lukenorrisf1 <crazydememphis@gmail.com>
2025-07-07 16:01:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+document.getElementById("root").innerHTML = `
+  <h1 style='text-align:center;color:green'>Bot WhatsApp Aluguel</h1>
+  <p style='text-align:center'>Clique abaixo para falar comigo no WhatsApp</p>
+  <div style='text-align:center;margin-top:20px'>
+    <a href='https://wa.me/5521982882464' target='_blank' style='
+      background:green;color:white;padding:10px 20px;text-decoration:none;border-radius:8px;display:inline-block'>
+      Falar no WhatsApp
+    </a>
+  </div>
+`;
